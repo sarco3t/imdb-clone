3d00ee56befc0bed8dbedd09bec7c99297c194bd
sarco3t <14gainward88@gmail.com>
2019-12-22 18:21:34 +0200

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'admin@admin.com', password: 'admin@admin.com', role: :admin)
+
+categories = (0..10).to_a.map do |n|
+  Category.create(title: "Category #{n}")
+end
+
+500.times do |n|
+  Movie.create(title: "movie #{n}", text: "description #{n}", category_id: categories.sample.id)
+end
