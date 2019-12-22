01cea17123c3f951ff012fad96e747096e053fb9
sarco3t <sarco3t@localhost.localdomain>
2019-12-22 14:41:13 +0200

    fixed turbolinks scrolling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,20 @@
 //= require rails-ujs
 //= require turbolinks
+
+window.scrollPosition = {x: 0, y: 0} // Our 'old' scroll position
+window.lastHref = null // Our 'old' page href
+
+// Before visit, simply store scroll position & url/href
+document.addEventListener('turbolinks:before-visit', function () {
+  window.scrollPosition = {x: window.scrollX, y: window.scrollY}
+  window.lastHref = window.location.href
+}, false)
+
+// After load
+document.addEventListener('turbolinks:load', function () {
+  // If we have a scroll position AND we're on the same page
+  if (window.scrollPosition && (this.location.href == window.lastHref)) {
+    // Scroll to our previous position
+    window.scrollTo(window.scrollPosition.x, window.scrollPosition.y)
+  }
+}, false)
