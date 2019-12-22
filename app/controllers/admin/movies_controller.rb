3f4a8b27b2cc0c76f8961d117e9ab3bca143e53f
sarco3t <sarco3t@localhost.localdomain>
2019-12-22 10:49:04 +0200

    completed admin

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,4 +1,4 @@
-class MoviesController < ApplicationController
+class Admin::MoviesController < AdminController
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
 
   # GET /movies
@@ -28,7 +28,7 @@ class MoviesController < ApplicationController
 
     respond_to do |format|
       if @movie.save
-        format.html { redirect_to @movie, notice: 'Movie was successfully created.' }
+        format.html { redirect_to [:admin, @movie], notice: 'Movie was successfully created.' }
         format.json { render :show, status: :created, location: @movie }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class MoviesController < ApplicationController
   def update
     respond_to do |format|
       if @movie.update(movie_params)
-        format.html { redirect_to @movie, notice: 'Movie was successfully updated.' }
+        format.html { redirect_to [:admin, @movie], notice: 'Movie was successfully updated.' }
         format.json { render :show, status: :ok, location: @movie }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class MoviesController < ApplicationController
   def destroy
     @movie.destroy
     respond_to do |format|
-      format.html { redirect_to movies_url, notice: 'Movie was successfully destroyed.' }
+      format.html { redirect_to admin_movies_url, notice: 'Movie was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -64,7 +64,7 @@ class MoviesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_movie
-      @movie = Movie.find(params[:id])
+      @movie = Movie.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/views/admin/movies/_movie.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! movie, :id, :title, :text, :category_id, :created_at, :updated_at
-json.url movie_url(movie, format: :json)
